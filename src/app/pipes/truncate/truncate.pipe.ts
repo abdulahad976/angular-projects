2034fda805f713e28eb283dcc07629b228fe7264
abdul-ahad <git@github.com:mughirarehman2573/office-management-system.git>
2024-11-20 18:26:17 +0500

    Add two Pipipes Truncate Values and Sort data in Accending and Decending Order

---
+++ src/app/pipes/truncate/truncate.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate',
+  standalone: true
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string = '', length: number = 100): string {
+    let availableCharacters = value.slice(0, length);
+
+    if(value.length > length) {
+      availableCharacters += '...';
+    }
+
+    return availableCharacters ;
+  }
+ 
+}
