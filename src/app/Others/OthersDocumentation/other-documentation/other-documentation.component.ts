2a9f989f4adb8549fc761eb0217c749e0acb81d0
abdul-ahad <git@github.com:mughirarehman2573/office-management-system.git>
2024-12-02 13:18:52 +0500

    Design a landing page about job Description

--- src/app/Others/OthersDocumentation/other-documentation/other-documentation.component.ts
+++ src/app/Others/OthersDocumentation/other-documentation/other-documentation.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../../components/card/card/card.component';
 import { FadeInOutComponent } from '../../components/fade-in-out/fade-in-out.component';
 import { AgGridComponent } from '../../components/ag-grid/ag-grid.component';
+import { ApplicationsComponent } from '../../applications/applications.component';
+import { ApplyFormComponent } from '../../apply-form/apply-form.component';
 
 @Component({
   selector: 'app-other-documentation',
   standalone: true,
-  imports: [CardComponent, FadeInOutComponent, AgGridComponent],
+  imports: [CardComponent, FadeInOutComponent, AgGridComponent, ApplicationsComponent, ApplyFormComponent],
   templateUrl: './other-documentation.component.html',
   styleUrl: './other-documentation.component.scss'
 })
