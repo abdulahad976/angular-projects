3fb3dc938ff779e164992d104c07ff214c4f18d9
abdul-ahad <git@github.com:mughirarehman2573/office-management-system.git>
2024-12-03 16:46:15 +0500

    Display the form data in another component create a services stored data in local storage within a service

--- src/app/Others/applications/applications.component.ts
+++ src/app/Others/applications/applications.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobApplication } from '../../Models/job-applications.interface';
+import { JobStorageService } from '../../services/JobStorage/job-storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-applications',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './applications.component.html',
   styleUrl: './applications.component.scss'
 })
-export class ApplicationsComponent {
+export class ApplicationsComponent implements OnInit {
+  applications: JobApplication[] = [];
 
+  constructor(private jobStorageService: JobStorageService) {}
+
+  ngOnInit(): void {
+    this.applications = this.jobStorageService.getApplications();
+    console.log(this.applications);
+  }
+
+  deleteApplication(index: number): void {
+    if (confirm('Are you sure you want to delete this application?')) {
+      this.applications.splice(index, 1);
+      this.jobStorageService.saveAllApplications(this.applications);
+    }
+  }
+
+  deleteAllApplications(): void {
+    if (confirm('Are you sure you want to delete all applications?')) {
+      this.applications = [];
+      this.jobStorageService.clearApplications();
+    }
+  }
+
+  
 }
